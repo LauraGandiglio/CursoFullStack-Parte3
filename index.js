9281f8dd1059a6109f7fa498af40ea2c58f521bf
LauraGandiglio <angyalsvart@gmail.com>
2024-04-20 02:16:32 -0300

    Adding gitignote, dotenv, mongo and models directory
    he information is provided by mongo db

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
+const Person = require("./models/person");
 
 //Midlewares
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(
   morgan((tokens, request, response) => {
     return [
@@ -53,9 +55,6 @@ const generateId = () => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  console.log(body);
-  console.log(body.name);
-  console.log(body.number);
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: "Name or number is missing",
@@ -82,7 +81,10 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  /*response.json(persons);*/
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -119,5 +121,5 @@ app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
